00854c89597be1b388bf12a4810c6006a0878fb7
Robert Gorz <rjgorz@gmail.com>
2023-01-10 09:32:36 -0600

    completed employee object and associated destructive and non-destructive functions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Robert",
+    streetAddress: "1234 1st Ave"
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newEmp = {...obj};
+    newEmp[key] = value;
+    return newEmp;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newEmp = {...obj};
+    delete newEmp[key];
+    return newEmp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
